a6969e118e84e708b73533d7409ddaa5155ed95c
Matt Saba <matavanff7@gmail.com>
2018-12-17 09:22:58 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,38 @@
+
+def instantiate_new_array
+  @my_new_array = []
+end
+
+def array_with_two_elements
+  @array_with_two_elements = ['one', 'two']
+end 
+
+def first_element(array)
+  return array[0]
+end 
+
+def third_element(array)
+  return array[2]
+end 
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+  first_element(array)
+end 
+
+def last_element_with_array_methods(array)
+  last_element(array)
+end 
+
+def length_of_array(array)
+  return array.length
+end 
+
+
+
+
+
+  
